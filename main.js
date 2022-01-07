57a65ad13ab09ed84c373cd04d4c5ef140e5796c
LuisVZ485 <juggerlavz23@gmail.com>
2022-01-06 20:36:23 -0500

    developing: init file js

---
+++ main.js
@@ -0,0 +1,41 @@
+const toggleMenu = document.querySelector('nav .toggle-menu');
+const navMenu = document.querySelector('nav .nav-menu');
+
+toggleMenu.addEventListener('click', function () {
+	navMenu.classList.toggle('show');
+})
+
+
+const allSection = document.querySelectorAll('header, section');
+const allLinkMenu = navMenu.querySelectorAll('a');
+
+
+allSection.forEach(section=> {
+	if((window.scrollY >= section.offsetTop - 64) && (window.scrollY <= (section.offsetTop - 64) + section.offsetHeight)) {
+		allLinkMenu.forEach(link=> {
+			if(link.getAttribute('href') === '#'+section.id) {
+				link.classList.add('active');
+			} else {
+				link.classList.remove('active');
+			}
+
+			link.addEventListener('click', function () {
+				navMenu.classList.remove('show');
+			})
+		})
+	}
+})
+
+window.addEventListener('scroll', function () {
+	allSection.forEach(section=> {
+		if((this.scrollY >= section.offsetTop - 72) && (this.scrollY <= (section.offsetTop + section.offsetHeight) - 64)) {
+			allLinkMenu.forEach(link=> {
+				if(link.getAttribute('href') === '#'+section.id) {
+					link.classList.add('active');
+				} else {
+					link.classList.remove('active');
+				}
+			})
+		}
+	})
+})
